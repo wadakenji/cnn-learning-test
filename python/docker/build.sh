2b264a5d7d49d96cd356d0c41dc895573cd66d39
wadakenji <ponporopen@gmail.com>
2025-10-18 15:03:15 +0900

    fix docker

--- python/docker/build.sh
+++ python/docker/build.sh
@@ -1,2 +1,1 @@
-docker image rm cnn-learning-test & \
 docker build . -t cnn-learning-test
--- python/docker/run.sh
+++ python/docker/run.sh
@@ -1,4 +1,5 @@
-docker run cnn-learning-test \
+docker run \
   --mount type=bind,src="$(pwd)"/src,dst=/cnn-learning-test/src \
   --name cnn-learning-test \
-  -it /bin/bash
+  -it \
+  cnn-learning-test
