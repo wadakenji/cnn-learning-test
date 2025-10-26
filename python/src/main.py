13a7089b3e4db64cbed9694ebb9f34633d8bb0ad
wadakenji <ponporopen@gmail.com>
2025-10-26 22:31:10 +0900

    update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -2,8 +2,11 @@ import numpy as np
 import _pickle,random
 from PIL import Image
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization
 
-IMAGE_SHAPE = (28, 28, 1)
+IMAGE_SHAPE = (32, 32, 3)
 BATCH_SIZE = 495
 
 # ファイルを読み込む
@@ -38,10 +41,30 @@ def get_images_and_labels(filename):
     images = vectors_to_images(vectors)
     return [images, labels]
 
+# 以下の記事で使用されていたモデル
+# https://www.yakupro.info/entry/cnn-regression
+def sample_cnn(input_shape):
+    model = Sequential()
+    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3),activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1, activation='linear'))
+    model.summary()
+    return model
+
 if __name__ == "__main__":
-    images_1, labels_1 = get_images_and_labels('src/data/cucumber-9-python/data_batch_1')
-    images_2, labels_2 = get_images_and_labels('src/data/cucumber-9-python/data_batch_2')
-    images_3, labels_3 = get_images_and_labels('src/data/cucumber-9-python/data_batch_3')
-    images_4, labels_4 = get_images_and_labels('src/data/cucumber-9-python/data_batch_4')
-    images_5, labels_5 = get_images_and_labels('src/data/cucumber-9-python/data_batch_5')
+    # images_1, labels_1 = get_images_and_labels('src/data/cucumber-9-python/data_batch_1')
+    # images_2, labels_2 = get_images_and_labels('src/data/cucumber-9-python/data_batch_2')
+    # images_3, labels_3 = get_images_and_labels('src/data/cucumber-9-python/data_batch_3')
+    # images_4, labels_4 = get_images_and_labels('src/data/cucumber-9-python/data_batch_4')
+    # images_5, labels_5 = get_images_and_labels('src/data/cucumber-9-python/data_batch_5')
     print(tf.__version__)
+    model = sample_cnn(IMAGE_SHAPE)
+    print(model)
