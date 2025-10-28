ccecbdbbbb6014ec3c8586e7f1d37097aa8f3b22
wadakenji <ponporopen@gmail.com>
2025-10-28 14:25:19 +0900

    test時に引数でmodelを指定できるように

--- python/src/test.py
+++ python/src/test.py
@@ -1,8 +1,18 @@
+import argparse
 from tensorflow.keras.models import load_model
 from data.get_data import images_test, labels_test
 
 if __name__ == "__main__":
-    model = load_model('src/models/sample_cnn/model.h5')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model')
+    args = parser.parse_args()
+
+    if (args.model is None or args.model == 'sample_cnn'):
+        model_dir = 'src/models/sample_cnn'
+    else:
+        raise Exception('invalid model arg')
+
+    model = load_model(model_dir + '/model.h5')
     score = model.evaluate(images_test, labels_test, verbose=1)
     print('Test loss: ', score[0])
     print('Test mae: ', score[1])
